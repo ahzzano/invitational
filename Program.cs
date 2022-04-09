ef702d4160ea60f28842b8ee3bb6a4a894e2cad1
ahzzano <w.santos.gg@gmail.com>
2022-04-09 20:13:51 +0800

    can login to discord

--- Program.cs
+++ Program.cs
@@ -1,14 +1,57 @@
 ﻿using System;
+using System.Text.Json;
 using Discord;
 using Discord.Net; 
+using Discord.WebSocket;
+using Discord.Commands;
+
+using System.Threading.Tasks;
 
 namespace invitational
 {
+    class SettingsFile {
+        public string token {get; set;}
+        public int maxPlayers {get; set;}
+        public int maxTeams {get; set;}
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+
+
+        public static Task Main(string[] args) => new Program().RunBotAsync();
+
+        private void LoadSettings() {
+
+            // Load the settings.json file
+            string settingsString = System.IO.File.ReadAllText("settings.json");
+            SettingsFile settingsValues = JsonSerializer.Deserialize<SettingsFile>(settingsString);
+
+            // Instantiate the Settings
+
+            Settings.Load(settingsValues);   
+        }
+
+        public async Task RunBotAsync()
         {
-            Console.WriteLine("Hello World!");
+            LoadSettings();
+
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            string token = Settings.GetDiscordToken();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage message) 
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
         }
     }
 }
--- Settings.cs
+++ Settings.cs
@@ -1,5 +1,32 @@
-class Settings {
-    static Settings instance = null; 
+namespace invitational {
+    class Settings {
+        public static Settings instance = null; 
 
-    
+        // Game Settings
+        public int maxPlayers;
+        public int maxTeams;
+
+        // Bot Settings
+        private string Token; 
+
+        private Settings() {
+            if(instance == null) {
+                Settings.instance = this; 
+            }
+        }
+
+        public static Settings Load(SettingsFile values) {
+            Settings settings = new Settings();
+            settings.Token = values.token;
+            settings.maxPlayers = values.maxPlayers;
+            settings.maxTeams = values.maxTeams;
+
+            return Settings.instance;
+        }
+
+        public static string GetDiscordToken() {
+            return instance.Token;
+        }
+
+    }
 }
